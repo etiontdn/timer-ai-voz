fc4456bd5912be6addb323c4e2324d8868f09529
Joao J <etiontdn@gmail.com>
2024-10-28 20:04:07 -0300

    adaptando prompt

--- recognition/api/server.js
+++ recognition/api/server.js
@@ -1,17 +1,18 @@
 const express = require('express')
-const genAi = require('genAi.js')
+const genAi = require('./genAi.js')
 const app = express()
 
-app.get('/interpret', async (req, res) => {
-    const data = req.body
-    const prompt = ```Traduza e interprete a frase fornecida no contexto de um sistema de gerenciamento de tarefas. 
+app.use(express.json())
+
+app.post('/interpret', async (req, res) => {
+    const prompt = `Traduza e interprete a frase fornecida no contexto de um sistema de gerenciamento de tarefas a voz, então tenha leniência com palavras que foram compreendidas errado pelo interpretador de voz. 
     Se a frase puder ser entendida como um comando para iniciar um timer,
-    responda somento o formato "Timer XX:YY - [Nome]",
+    responda somento o formato "Timer XX:YY - [Nome]" (XX:  minutos e YY: segundos),
     sem nenhuma analise adicional.
     Caso contrário, responda apenas "no". Por exemplo,
     para a frase 'estudar por 30 minutos',
     o sistema poderia retornar 'Timer 30:00 - Estudar'.
-    Não analise a solicitação, responda somente no formato especificado na primeira linha lavar roupa 10 minutos\n```
+    Não analise a solicitação, responda somente no formato especificado na primeira linha\n`
     + req.body.phrase;
     const response = await genAi.sendPrompt(prompt)
     res.json({response})

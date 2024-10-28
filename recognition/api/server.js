ee7b239841de2d3e8f225f5f4d63a346d25eb96f
Joao J <etiontdn@gmail.com>
2024-10-28 19:46:10 -0300

    inicio api do gemini ai do google

--- recognition/api/server.js
+++ recognition/api/server.js
@@ -1,15 +1,20 @@
 const express = require('express')
+const genAi = require('genAi.js')
 const app = express()
 
-/*
-app.use(function (req, res, next) {
-    console.log('middleware 1')
-    next()
-})*/
-
-app.get('/interpret', (req, res) => {
+app.get('/interpret', async (req, res) => {
     const data = req.body
-
+    const prompt = ```Traduza e interprete a frase fornecida no contexto de um sistema de gerenciamento de tarefas. 
+    Se a frase puder ser entendida como um comando para iniciar um timer,
+    responda somento o formato "Timer XX:YY - [Nome]",
+    sem nenhuma analise adicional.
+    Caso contrário, responda apenas "no". Por exemplo,
+    para a frase 'estudar por 30 minutos',
+    o sistema poderia retornar 'Timer 30:00 - Estudar'.
+    Não analise a solicitação, responda somente no formato especificado na primeira linha lavar roupa 10 minutos\n```
+    + req.body.phrase;
+    const response = await genAi.sendPrompt(prompt)
+    res.json({response})
 })
 
 app.listen(3000, () => {
